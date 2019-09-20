930e9b6ad8495793121f6b2f34e8c13e55ae9374
Joey Castillo <jose.castillo@gmail.com>
2019-09-19 20:30:03 -0400

    eliminate external dependency

--- src/BabelSPIFlash.h
+++ src/BabelSPIFlash.h
@@ -10,19 +10,24 @@
 #define BabelSPIFlash_h
 
 #include <stdio.h>
+#include "SPI.h"
 #include "BabelDevice.h"
-#include "Adafruit_SPIFlash.h"
 
 class BabelSPIFlash: public BabelDevice {
 public:
     BabelSPIFlash(uint8_t ss, SPIClass *spi);
-    ~BabelSPIFlash();
     void begin();
     void read(uint32_t addr, void *data, uint32_t len);
 private:
     uint8_t cs;
     SPIClass *spi;
-    Adafruit_SPIFlash *flash;
+    SPISettings settings;
+
+    void read(uint32_t addr, uint8_t *data, uint32_t len);
+    bool runCommand(uint8_t command);
+    bool readCommand(uint8_t command, uint8_t* response, uint32_t len);
+    uint8_t readStatus();
+    uint8_t readStatus2(void);
 };
 
 #endif /* BabelSPIFlash_h */
