7ba8dc9a55acc1a5163522918b98f9059af1c7bd
Joey Castillo <jose.castillo@gmail.com>
2020-08-20 12:59:40 -0400

    WIP: babel to lvgl glue

---
+++ src/lv_babel.h
@@ -0,0 +1,26 @@
+/**
+ * @file lv_babel.h
+ *
+ */
+#ifndef _LV_FONT_BABEL_H
+#define _LV_FONT_BABEL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "lvgl.h"
+#include "BabelDevice.h"
+
+int lv_babel_init(BabelDevice *babel);
+int lv_babel_font_init(lv_font_t * font);
+
+/**********************
+ *      MACROS
+ **********************/
+ 
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif
